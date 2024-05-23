4440bca1e3e5874bd34c0d22b5bed0f6c94bdb4c
wendymasese <wendy.masese@student.moringaschool.com>
2024-05-23 19:51:08 +0300

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  const handleClick = () => {
+    setIsInCart((prevIsInCart) => !prevIsInCart);
+  };
+
+  const itemClass = isInCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick}>
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
